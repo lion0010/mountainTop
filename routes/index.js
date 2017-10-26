716a25851f6016b58f109e9e906b7ebc9cf05647
wqj19980925 <1714796246@qq.com>
2017-10-26 09:06:48 +0800

    jk
    ke

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
-
+router.get('/account', function(req, res, next) {
+    res.render('account', { title: 'Express' });
+});
 module.exports = router;
